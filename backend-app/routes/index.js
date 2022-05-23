9a10a9a4d2cc088395055d45f0b9fd433d23f429
Karthik Avula <96226768+karthikavula-wal@users.noreply.github.com>
2022-05-23 12:16:12 +0530

    Update index.js

--- backend-app/routes/index.js
+++ backend-app/routes/index.js
@@ -1,6 +1,9 @@
 var express = require('express');
 const path = require('path');
 var router = express.Router();
+var usersRouter = require('../routes/users');
+var departmentRouter = require('../routes/department');
+var employeesRouter = require('../routes/employee');
 
 /* GET home page. */
 router.get('/', function (req, res, next) {
@@ -10,4 +13,8 @@ router.get('/uploads/:name', (req, res) => {
     const filePath = path.join(__dirname, '../', `/uploads/${req.params.name}`);
     res.status(200).sendFile(filePath);
 });
+
+router.use('/users', usersRouter);
+router.use('/departments', departmentRouter);
+router.use('/employees', employeesRouter);
 module.exports = router;
